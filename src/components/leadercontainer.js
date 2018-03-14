1cd7c7ebd911f6b8dc6f5f9a70d36fc6188c9b09
Stanley Huang <infamous_godhand@yahoo.com>
2018-03-14 11:29:50 +0000

    able to display the data for recent top ranking campers in table form

--- src/components/leadercontainer.js
+++ src/components/leadercontainer.js
@@ -22,7 +22,7 @@ class LeaderContainer extends React.Component{
         console.log("Mounted");
         const first=await axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
         .then(function (response) {
-            console.log("setstate1");
+            console.log("getting recent top campers");
             return response;
             //this.setState({recent_data:response});
          })
@@ -32,7 +32,7 @@ class LeaderContainer extends React.Component{
         
         const second=await axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
         .then(function (response) {
-            console.log("setstate2");
+            console.log("getting all time top campers");
             return response;
             
             //this.setState({alltime_data:response});
@@ -40,16 +40,48 @@ class LeaderContainer extends React.Component{
         .catch(function (error) {
             console.log(error);
         });
-        console.log(first);
-        console.log(second);
+        //console.log(first);
+        //console.log(second);
         this.setState({recent_data:first,alltime_data:second})
     }
     render(){
+        
+        var testList=[];
+        var username;
+        var recentPts;
+        var alltimePts;
+        var spot;
+        
+        if(this.state.recent_data.length!==0){
+            for(var i=0;i<5;i++){
+                console.log("recent-->"+this.state.recent_data.data[i].username);
+                username=this.state.recent_data.data[i].username;
+                alltimePts=this.state.recent_data.data[i].alltime;
+                recentPts=this.state.recent_data.data[i].recent;
+                spot=i;
+                testList.push(
+                        <tr>
+                            <td>{spot}</td>
+                            <td>{username}</td>
+                            <td>{recentPts}</td>
+                            <td>{alltimePts}</td>
+                        </tr>
+                
+                )
+            }
+        }
+        
+        if(this.state.alltime_data.length!==0){
+            for(i=0;i<5;i++){
+                console.log("all time-->"+this.state.alltime_data.data[i].username);
+            }
+        }
+        
        
         return (
         <div>
             <Header />
-            <LeaderTable />
+            <LeaderTable recent={testList}/>
         </div>
             );
     }
--- src/components/leadertable.js
+++ src/components/leadertable.js
@@ -2,10 +2,22 @@ import React from "react";
 import "./leadertable.css";
 
 class LeaderTable extends React.Component{
+    
     render(){
         return ( 
                 <div className="tableWrapper leader">
                     <h2 className="leader">Leader Board</h2>
+                    <table>
+                        <tr>
+                            <th>#</th>
+                            <th>Camper</th>
+                            <th>Last 30 days</th>
+                            <th>All Time</th>
+                            
+                            
+                        </tr>
+                        {this.props.recent}
+                    </table>
                 </div>
             );
     }
